deceac82833cd62745216acd08493847ee555429
sanjaychikkala <sanjaychikkala17@gmail.com>
2024-07-17 16:04:17 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+// src/App.js
+import React from 'react';
+import TodoList from './TodoList';
+
+const App = () => {
+    return (
+        <div>
+            <TodoList />
+        </div>
+    );
+};
+
+export default App;
+
+
